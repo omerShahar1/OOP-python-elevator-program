ca87832688157d2a5e7b50fc4713e626b020a728
omer <omerzshahar@gmail.com>
2021-11-09 20:10:10 +0200

    start project

---
+++ Assignments/Ex1/elevator.py
@@ -0,0 +1,13 @@
+class Elevator(object):
+    def __init__(self, id:int, speed:float, minFloor:int, maxFloor:int, closeTime:float, openTime:float, startTime:float, stopTime:float):
+        self.id = id
+        self.speed = speed
+        self.minFloor = minFloor;
+        self.maxFloor = maxFloor;
+        self.closeTime = closeTime
+        self.openTime = openTime;
+        self.startTime = startTime;
+        self.stopTime = stopTime;
+
+
+
