305a0878f735ae195386bbe6d3fd579a4a6f2f67
omer <omerzshahar@gmail.com>
2021-11-17 00:42:46 +0200

    make changes to the algorithm

--- Ex1_main.py
+++ Ex1_main.py
@@ -1,8 +1,6 @@
 import json
 import csv
 import sys
-
-import pandas
 from call import Call
 from elevator import Elevator
 from building import Building
@@ -11,10 +9,10 @@ from building import Building
 # return the id of the best elevator to use. also insert the call to the elevator calls list (and its data to the other lists)
 def best_elevator(b: Building, call: Call) -> int:
     if len(b.elevators) == 1:
-        Elevator.add_call(b.elevators[0], call)
         return 0
     answer = 0
-    bestTime = b.elevators[0].time
+    bestTime = 0
+    firstTime = True
 
     for elev in b.elevators:
         list1 = elev.upList.copy()
@@ -22,11 +20,18 @@ def best_elevator(b: Building, call: Call) -> int:
         list3 = elev.newUpList.copy()
         list4 = elev.newDownList.copy()
 
-        elevator_time(elev, call)
-
-        if elev.time < bestTime:
+        if firstTime:
+            firstTime = False
+            elevator_time(elev, call)
             answer = elev.id
             bestTime = elev.time
+        else:
+            elevator_time(elev, call)
+            if elev.time < bestTime:
+                answer = elev.id
+                bestTime = elev.time
+        # print(elev.id, ": ", elev.time)
+        # print(b.elevators[0].upList)
         Elevator.zero_data(elev)
         elev.upList = list1.copy()
         elev.downList = list2.copy()
@@ -47,7 +52,14 @@ def elevator_time(elevator: Elevator, call: Call):
 
 
 def case_0(elevator: Elevator, call: Call):
-    pass
+    if elevator.current == call.src:
+        elevator.time = elevator.time + elevator.openTime + elevator.closeTime
+    else:
+        move(elevator, elevator.current, call.src)
+        elevator.current = call.src
+
+    move(elevator, elevator.current, call.dest)
+    elevator.current = call.dest
 
 
 def case_2(elevator: Elevator, call: Call):
@@ -121,37 +133,26 @@ def move(elev: Elevator, current: int, next: int):
         dist = current - next
     else:
         dist = next - current
-    elev.time = elev.time + (dist / elev.speed) + elev.startTime + elev.stopTime + elev.closeTime + elev.stopTime
+    elev.time = elev.time + (dist / elev.speed) + elev.startTime + elev.stopTime + elev.closeTime + elev.openTime
 
 
-if __name__ == '__main__':
-    args1 = sys.argv[1]
-    args2 = sys.argv[2]
-    args3 = sys.argv[3]
-    # opening JSON file (Building.json) and load it in to a dictionary (data)
-    with open(args1) as json_file:
-        data = json.load(json_file)
-        # create building b with the data from the json file
-        b = Building(data['_minFloor'], data['_maxFloor'])
-        # add elevators to the building object
-        for i in data['_elevators']:
-            elev = Elevator(i['_id'], i['_speed'], i['_minFloor'], i['_maxFloor'], i['_closeTime'], i['_openTime'],
-                            i['_startTime'], i['_stopTime'])
-            Building.add_elevator(b, elev)
+args1 = sys.argv[1]
+args2 = sys.argv[2]
+args3 = sys.argv[3]
+# opening JSON file (Building.json) and load it in to a dictionary (data)
 
-    # open csv file (calls)
-    calls_file = open(args2, 'r')
+b = Building(args1)
+
+# open csv file (calls)
+with open(args2, 'r') as calls_file:
     csv_reader = csv.reader(calls_file)
-    output = pandas.read_csv(args3)
-    count = 0
-    # go over all the lines in the csv file
-    for row in csv_reader:
-        if b.maxFloor >= int(row[2]) >= b.minFloor and b.maxFloor >= int(row[3]) >= b.minFloor:
+    with open(args3, "w", newline='\n') as output:
+        csvWriter = csv.writer(output)
+        # go over all the lines in the csv file
+        for row in csv_reader:
             call = Call(float(row[1]), int(row[2]), int(row[3]))
             answer = best_elevator(b, call)
             row[5] = answer
-        output.loc[count] = row
-        count = count + 1
-    output.to_csv(args3, index=False)
+            csvWriter.writerow(row)
 
-    calls_file.close()
+
